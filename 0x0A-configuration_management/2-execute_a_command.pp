1c9b8a83806724795654e832700809292e3c1443
Joshua <85683603+Josh-dev69@users.noreply.github.com>
2023-11-26 12:40:04 +0100

    Updated Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # kill_process.pp
 
-exec { 'killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
+exec { 'kill process':
+  command  => 'pkill killmenow',
+  path     => '/usr/bin',
+  provider => shell
 }
