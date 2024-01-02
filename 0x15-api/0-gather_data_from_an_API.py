6ba8ea9938c9246a6c57505adc7954988e45b8cb
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-02 12:23:49 +0100

    My First API practice

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,32 +4,26 @@
 import sys
 import requests as req
 
-# Retrieve user ID from command-line arguments
 user_id = sys.argv[1]
 
-# URLs for user and todo data
 url = "https://jsonplaceholder.typicode.com/users/{}"
 url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"
 
 user_url = url.format(user_id)
 todo_url = url2.format(user_id)
 
-# Fetch user data
 user_data = req.get(user_url).json()
 username = user_data.get("name")
 
-# Fetch user's todos
 user_todos = req.get(todo_url).json()
 total = len(user_todos)
 
-# Filter completed todos
 completed_todos = []
 for todo in user_todos:
     if todo['completed']:
         completed_todos.append(todo)
 completed = len(completed_todos)
 
-# Print task details
 print("Employee {} is done with tasks({}/{})"
         .format(username, completed, total))
 for todo in completed_todos:
