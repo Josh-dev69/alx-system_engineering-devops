db395227f54c2a29f5cd02ab5fdf9b13dc28944e
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-02 13:48:45 +0100

    My First API practice

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """
 Employee Task Summary Script
 
-This script retrieves information about a user and their completed tasks from the JSONPlaceholder API.
-It takes a user ID as a command-line argument, fetches the user's details, and prints a summary of
-completed tasks along with their titles.
+This script retrieves information about a user and their
+completed tasks from the JSONPlaceholder API. It takes a user ID
+as a command-line argument, fetches the user's details, and
+prints a summary of completed tasks along with their titles.
 
 Usage:
     python3 0-gather_data_from_an_API.py <user_id>
@@ -22,7 +23,7 @@ user_url = url.format(user_id)
 todo_url = url2.format(user_id)
 
 user_data = req.get(user_url).json()
-username = user_data.get("name")
+user = user_data.get("name")
 
 user_todos = req.get(todo_url).json()
 total = len(user_todos)
@@ -33,7 +34,6 @@ for todo in user_todos:
         completed_todos.append(todo)
 completed = len(completed_todos)
 
-print("Employee {} is done with tasks({}/{})"
-        .format(username, completed, total))
+print(f"Employee {user} is done with tasks({completed}/{total}")
 for todo in completed_todos:
     print("     {}".format(todo['title']))
