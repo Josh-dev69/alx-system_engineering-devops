9185a7ebac16cd805416e3ca13cd12a8ba323348
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-02 16:05:22 +0100

    My First API practice

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -43,12 +43,7 @@ def get_user_todos(user_id):
 
 def main():
     """Main function to execute the script."""
-    if len(sys.argv) > 1:
-        user_id = sys.argv[1]
-
-    if not user_id:
-        print("Please provide a user ID as a command-line argument.")
-        sys.exit(1)
+    user_id = sys.argv[1]
 
     user_data = get_user_data(user_id)
     username = user_data.get("name")
