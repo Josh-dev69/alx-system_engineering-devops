1974772c98c6eefa75e352a24110f5192f95d432
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-03 23:20:07 +0100

    Export to a JSON file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,60 @@
-#!/usr/bin/env python3
-""" A script that exports an employee's todo list to JSON """
+#!/usr/bin/python3
 
-import sys
-import requests as req
+"""
+This script retrieves information about a user and their completed
+tasks from the JSONPlaceholder API and exports the data in both CSV and JSON formats.
+"""
+
 import json
+import requests
+import sys
 
-# Retrieve user ID from command-line arguments
-user_id = sys.argv[1]
+def get_user_data(user_id):
+    """
+    Fetches user data from the JSONPlaceholder API.
+    """
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    return requests.get(user_url).json()
 
-# URLs for user and todo data
-user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+def get_user_todos(user_id):
+    """
+    Fetches user's to-do list from the JSONPlaceholder API.
+    """
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    return requests.get(todo_url).json()
 
-user_data = req.get(user_url).json()
-username = user_data.get("username")
 
-user_todos = req.get(todo_url).json()
+def export_to_json(user_id):
+    """
+    Exports user's completed tasks to a JSON file
+    """
+    user_data = get_user_data(user_id)
+    username = user_data.get("username")
 
-# Filter completed todos
-completed_todos = []
-for todo in user_todos:
-    if todo['completed']:
-        completed_todos.append({
-            'task': todo['title'],
-            'completed': todo['completed'],
-            'username': username
-        })
+    user_todos = get_user_todos(user_id)
+    completed_todos = [
+        {
+            "task": todo["title"],
+            "completed": todo["completed"],
+            "username": username,
+        }
+        for todo in user_todos
+    ]
 
-# Create JSON data
-json_data = {user_id: completed_todos}
+    filename = f"{user_id}.json"
+    with open(filename, "w", encoding="utf-8") as jsonfile:
+        json.dump({user_id: completed_todos}, jsonfile)
 
-# Write data to JSON file
-json_filename = f"{user_id}.json"
-with open(json_filename, 'w') as json_file:
-    json.dump(json_data, json_file)
+    print(f"JSON data exported to {filename}")
 
-print(f"JSON data exported to {json_filename}")
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    user_id = sys.argv[1]
+    export_to_json(user_id)
+
+
+if __name__ == "__main__":
+    main()
