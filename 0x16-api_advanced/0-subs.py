3f4889ba52543ed4861b29c472c67f413a222d26
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-16 02:13:05 +0100

    Get total subscriber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,9 @@ def number_of_subscribers(subreddit):
     Funtion that queries the Reddit API.
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers)
-    if response.status_code >= 300:
+    if (not response.ok):
         return 0
     subscriber_count = response.json().get('data').get('subscribers')
     return subscriber_count
